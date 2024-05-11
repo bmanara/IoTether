307f380a9f83c601a7148343facd2b31ebcb92e1
bmanara <bmacraze@gmail.com>
2024-05-11 14:21:47 +0800

    Add animation to player
    Idle and walking animations added.

--- IoTether/Assets/Scripts/PlayerController.cs
+++ IoTether/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float moveSpeed = 2f;
+    private float moveSpeed = 4f;
+    private bool facingRight = true;
 
     private Vector2 input;
     public Rigidbody2D rbPlayer;
@@ -16,11 +17,40 @@ public class PlayerController : MonoBehaviour
         // Get movement input
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
+
+        // Flip sprite based on movement direction
+        if (input.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (input.x < 0 && facingRight)
+        {
+            Flip();
+        }
+        
+
+        // Update animator parameters
+        if (input != Vector2.zero)
+        {
+            animator.SetBool("isMoving", true);
+        } 
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
     }
 
     private void FixedUpdate()
     {
         // Movement logic
         rbPlayer.MovePosition(rbPlayer.position + input * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        facingRight = !facingRight;
     }
 }
