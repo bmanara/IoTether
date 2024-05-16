eb15d6b2b903ae78f8a78a3d6512fbc0fea6a69d
bmanara <bmacraze@gmail.com>
2024-05-16 10:48:34 +0800

    Fixed weapon switching bug
    Player prefab created with new script and parent object to encapsulate

--- IoTether/Assets/Scripts/WeaponScripts/WeaponPickup.cs
+++ IoTether/Assets/Scripts/WeaponScripts/WeaponPickup.cs
@@ -5,18 +5,44 @@ using UnityEngine;
 public class WeaponPickup : MonoBehaviour
 {
     public GameObject weaponToGive;
+    private bool ableToPickup = false;
+    private Collider2D other;
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) {
-            // Add UI to show player that they can pick up the weapon
-            Debug.Log("Colliding with player");
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                Debug.Log("Pressing E");
-                collision.GetComponent<WeaponSwap>().UpdateWeapon(weaponToGive);
-                Destroy(gameObject);
-            }
+        if (collision.CompareTag("Player"))
+        {
+            ableToPickup = true;
+            other = collision;
         }
     }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            ableToPickup = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (ableToPickup && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Pressing E");
+            other.GetComponent<WeaponSwap>().UpdateWeapon(weaponToGive);
+            Destroy(gameObject);
+        }
+    }
+
+    /*
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.E) && collision.CompareTag("Player")) {
+            Debug.Log("Pressing E");
+            collision.GetComponent<WeaponSwap>().UpdateWeapon(weaponToGive);
+            Destroy(gameObject);
+        }
+    }
+    */
 }
