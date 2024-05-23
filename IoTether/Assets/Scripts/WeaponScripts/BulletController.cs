3d7c1f51c140abec2418643b5a9a9b9567add3bf
gnoossk <143413957+gnoossk@users.noreply.github.com>
2024-05-23 20:50:11 +0800

    Update BulletController with easier Impact() function

--- IoTether/Assets/Scripts/WeaponScripts/BulletController.cs
+++ IoTether/Assets/Scripts/WeaponScripts/BulletController.cs
@@ -7,26 +7,29 @@ public class BulletController : MonoBehaviour
     private int damage = 1;
     private float knockbackForce = 10f;
     public GameObject impactEffect;
+
+    private void Impact()
+    {
+        Destroy(gameObject);
+        Instantiate(impactEffect, transform.position, transform.rotation);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
             // All enemies will be damageable... right?
             IDamageable damageable = collision.GetComponent<IDamageable>();
-            Destroy(gameObject);
+            Impact();
             EnemyController enemy = collision.gameObject.GetComponent<EnemyController>();
 
             Vector2 direction = (collision.transform.position - transform.position).normalized;
             Vector2 knockback = direction * knockbackForce;
             damageable.DecreaseHealth(damage, knockback);
-            Instantiate(impactEffect, transform.position, transform.rotation); 
         }
         else if (collision.gameObject.tag != "Player"
             && collision.gameObject.tag != "Spike")
         {
-            Destroy(gameObject);
-            Instantiate(impactEffect, transform.position, transform.rotation);
-
+            Impact();
         }
 
         Destroy(gameObject, 2f);
