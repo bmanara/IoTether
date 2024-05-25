593adaf5197f085ddb5fe7f5341c0370f4da3fc6
bmanara <bmacraze@gmail.com>
2024-05-26 07:41:12 +0800

    Dave can now talk

--- IoTether/Assets/Scripts/DialogueTrigger.cs
+++ IoTether/Assets/Scripts/DialogueTrigger.cs
@@ -5,6 +5,31 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+    private bool ableToTalk = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            ableToTalk = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            ableToTalk = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (ableToTalk && Input.GetKeyDown(KeyCode.E))
+        {
+            TriggerDialogue();
+        }
+    }
 
     public void TriggerDialogue()
     {
