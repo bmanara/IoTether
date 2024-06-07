22cebed91d5d0aba76ac9af24f28312b6b18b421
gnoossk <143413957+gnoossk@users.noreply.github.com>
2024-06-07 15:35:35 +0800

    Add BossTrigger.cs

---
+++ IoTether/Assets/BossTrigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTrigger : MonoBehaviour
+{
+   private bool isTriggered = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+         if (other.CompareTag("Player") && !isTriggered)
+        {
+              isTriggered = true;
+              // Trigger Boss Fight
+         }
+    }
+}
