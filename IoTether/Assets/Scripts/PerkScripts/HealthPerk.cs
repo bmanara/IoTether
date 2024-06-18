9becbc41d4784ab4cabe5303c398cd7e4dbdcc06
gnoossk <143413957+gnoossk@users.noreply.github.com>
2024-06-18 16:21:32 +0800

    Add IPerk and HealthPerk objects

---
+++ IoTether/Assets/Scripts/PerkScripts/HealthPerk.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HealthPerk", menuName = "Perks/HealthPerk")]
+public class HealthPerk : IPerk
+{
+    public int amount;
+    public override void Apply(GameObject target)
+    {
+       GameObject playerParent = target.transform.parent.gameObject;
+        playerParent.GetComponent<PlayerHealth>().IncrementHealth(amount);
+        
+    }
+}
