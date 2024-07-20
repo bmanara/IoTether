5e2ade6f9f7c70471c85315f852c4b7a85b647e4
bmanara <bmanaranus@gmail.com>
2024-07-20 15:42:23 +0800

    Give Chris Special Attack
    SFX added as well

--- IoTether/Assets/Scripts/EnemyScripts/ChrisController.cs
+++ IoTether/Assets/Scripts/EnemyScripts/ChrisController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEngine;
 
 public class ChrisController : Enemy
@@ -9,6 +10,8 @@ public class ChrisController : Enemy
     public BossBattle bossBattleLogic;
 
     private string bossName = "Chris";
+    private int numberOfProjectiles = 15;
+    private float radius = 5f;
 
     private bool isEnraged = false;
 
@@ -17,12 +20,11 @@ public class ChrisController : Enemy
         base.Init();
 
         health = 160;
-        damage = 2;
+        damage = 1;
 
         energyDrop = 160;
         healthDrop = 2;
 
-        // TODO: Give firepoint to Chris
         firePoint = transform.Find("FirePoint");
 
         UIManager.manager.SetBossHealth(health, 160);
@@ -48,11 +50,12 @@ public class ChrisController : Enemy
     private void Enrage()
     {
         isEnraged = true;
-        // TODO: Set up animator
         GetComponent<Animator>().SetTrigger("isEnraged");
+
         Color tmp = Color.red;
         tmp.a = 0.5f;
         GetComponent<SpriteRenderer>().color = tmp;
+        numberOfProjectiles = 20;
     }
 
     public override void DecreaseHealth(int damage)
@@ -69,11 +72,38 @@ public class ChrisController : Enemy
 
     public void Attack()
     {
-        Debug.Log("Chris Attacks! RAAAAAH");
+        // Debug.Log("Chris Attacks! RAAAAAH");
+        StartCoroutine("Shoot");
+        GetComponent<AudioSource>().Play();
     }
 
     public void EnragedAttack()
     {
-        Debug.Log("Angry Chris Attacks! RAAAAAH");
+        // Debug.Log("Angry Chris Attacks! RAAAAAH");
+        StartCoroutine("Shoot");
+        GetComponent<AudioSource>().Play();
+    }
+
+    private IEnumerator Shoot()
+    {
+        float angleStep = 360f / numberOfProjectiles;
+        float angle = 0f;
+
+        for (int i = 0; i < numberOfProjectiles; i++)
+        {
+            float projectileDirX = firePoint.position.x + Mathf.Sin((angle * Mathf.PI) / 180) * radius;
+            float projectileDirY = firePoint.position.y + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
+
+            Vector2 projectileVector = new Vector2(projectileDirX, projectileDirY);
+            Vector2 projectileMoveDir = (projectileVector - (Vector2)firePoint.position).normalized * 8f;
+
+            GameObject bullet = EnemyBulletController.Create(projectile, firePoint, damage);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector2(projectileMoveDir.x, projectileMoveDir.y);
+
+            angle += angleStep;
+
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
