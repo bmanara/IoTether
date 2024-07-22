1010b6719e962e1be87ed198fe04492557e688d9
gnoossk <143413957+gnoossk@users.noreply.github.com>
2024-07-22 16:22:27 +0800

    Change PlayerHealth to work for Unit Testing

--- IoTether/Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ IoTether/Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -70,7 +70,7 @@ public class PlayerHealth : MonoBehaviour, IDamageable
         }
         
         Invoke("ResetMaterial", .1f);
-        AudioManager.manager.PlaySFX("PlayerHit");
+        GetComponent<AudioSource>().Play();
     }
 
     private void Die()
