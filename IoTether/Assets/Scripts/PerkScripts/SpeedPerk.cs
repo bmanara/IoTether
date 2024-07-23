39ed132e3f370704248176f7630865d72f7b12e9
gnoossk <143413957+gnoossk@users.noreply.github.com>
2024-07-23 17:05:45 +0800

    Update SpeedPerk

--- IoTether/Assets/Scripts/PerkScripts/SpeedPerk.cs
+++ IoTether/Assets/Scripts/PerkScripts/SpeedPerk.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SpeedPerk", menuName = "Perks/SpeedPerk")]
 public class SpeedPerk : IPerk
 {
-    public float amount;
+    public float Speed;
 
     public override void Apply(GameObject target)
     {
         GameObject playerParent = target.transform.parent.gameObject;
-        playerParent.GetComponent<PlayerControllers>().IncreaseMoveSpeed(amount);
+        playerParent.GetComponent<PlayerControllers>().IncreaseMoveSpeed(Speed);
     }
 }
