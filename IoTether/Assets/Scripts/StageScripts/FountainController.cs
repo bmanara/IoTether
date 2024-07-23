86adff5067383f311f23954c4e5d1d474f36fc63
gnoossk <143413957+gnoossk@users.noreply.github.com>
2024-07-23 16:52:23 +0800

    Rename Scripts

--- IoTether/Assets/Scripts/StageScripts/FountainController.cs
+++ IoTether/Assets/Scripts/StageScripts/FountainController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FountainScript : InteractableProp
+public class FountainController : InteractableProp
 {
     public GameObject refillPerk;
     
